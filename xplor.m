2eb03e54d7d40e18a96d5c88a360af35bccb9841
remi <remi.girard@ntymail.com>
2019-08-30 16:29:24 +0200

    add comments

--- xplor.m
+++ xplor.m
@@ -1,18 +1,17 @@
 function V = xplor(data,varargin)
-% function V = xplor(data,varargin)
-    % function V = xplor(data)
-    % function V = xplor()         [demo data]
-    %---
-    % Base xplor starter
-    %
-    % Input:
-    % - data        data to visualize 
-    % - varargin    number of arguments in input
-    %
-    % 
-% create a demo data
+% function V = xplor(data)
+% function V = xplor()         [demo data]
+% ---
+% xplor starter
+%
+% Input:
+% - data        data to visualize 
+% - varargin    1-by-N cell array, where N is the number of inputs that
+% the function receives after the parameter data
+
+% create a demo xdata with header
 if nargin==0
-    [~,~,~,dat] = flow; 
+    [~,~,~,dat] = flow;
     dat = permute(dat,[1 3 2]);
     s = size(dat);
     head = xplr.header({'x' 'px' s(1)},{'y' 'px' s(2)},{'axis' num2cell(['a':'y' 'A':'Y'])});
@@ -20,11 +19,14 @@ if nargin==0
 end
 
 % create headers and launch view
+% if a parameter data is present -> evaluate it (execute and get result)
 if ischar(data)
     name = data;
     data = evalin('base',name);
+% if no parameters are presents (same case as previous create a demo data)
 elseif nargin==0
     name = 'demo data';
+% TODO: document this case
 else
     name = inputname(1);
 end
