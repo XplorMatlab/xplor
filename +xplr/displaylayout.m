3201aa5155df3f8a5065963fd050faafc13bf1da
thomas bureau <thomas.deneux@unic.cnrs-gif.fr>
2020-03-04 10:07:38 +0100

    fixed bugs with labels, renamed "org" `viewdisplay` property as "layout", and made it an object of new `displaylayout` class

---
+++ +xplr/displaylayout.m
@@ -0,0 +1,61 @@
+classdef displaylayout
+    
+    % This is a simple class that defines where each dimension is displayed
+    % in XPLOR display.
+    
+    properties
+        % parent display
+        D
+        % layout 
+        x
+        y
+        ystatic
+        xy
+        yx
+    end
+    properties (Dependent, SetAccess='private')
+        dim_locations
+    end
+    
+    % Create
+    methods
+        function L = displaylayout(D)
+            % parent display
+            L.D = D;
+            
+            % choose a default organization for display D
+            nd = D.nd;
+            doimage = strcmp(D.displaymode,'image');
+            if D.nd == 3 && doimage
+                L.x = 1;
+                L.y = 2;
+                L.xy = 3;
+            else
+                L.x = 1:2:nd;
+                L.y = 2:2:nd;
+            end
+        end
+    end
+    
+    % Manipulation
+    methods
+        function pos = get.dim_locations(L)
+            pos = cell(1,L.D.nd);
+            F = {'x', 'y', 'ystatic', 'xy', 'yx'};
+            for i=1:length(F)
+                f = F{i};
+                pos(L.(f)) = {f}; 
+            end
+        end
+        function L2 = strip_singleton(L, D)
+            L2 = L; % copy (displaylayout is not a handle class)
+            singleton = (L.D.slice.sz == 1);
+            L2.x(singleton(L2.x)) = [];
+            L2.y(singleton(L2.y)) = [];
+            L2.ystatic(singleton(L2.ystatic)) = [];
+            L2.xy(singleton(L2.xy)) = [];
+            L2.yx(singleton(L2.yx)) = [];
+        end
+    end
+    
+end
