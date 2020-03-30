0cecf3f59623b7766e97a336478a45cdf6e0e5a6
Thomas laptop <thomas.deneux@cnrs.fr>
2020-03-30 18:41:34 +0200

    <commented changes: drag & drop to xplor, not working yet>

--- xplor.m
+++ xplor.m
@@ -1,5 +1,5 @@
 function V = xplor(data,varargin)
-% function V = xplor(data)
+% function V = xplor(data[,dimIDs,filters])
 % function V = xplor()         [demo data]
 % ---
 % xplor starter
@@ -17,6 +17,13 @@ if nargin==0
     head = xplr.header({'x' 'px' s(1)},{'y' 'px' s(2)},{'axis' num2cell(['a':'y' 'A':'Y'])});
     data = xplr.xdata(dat,head,'Flow Data');
 end
+
+% % drag & drop window
+% if nargin==0
+%     xplor_window()
+%     return
+% end
+
 
 % create headers and launch view
 % if a parameter data is present -> evaluate it (execute and get result)
@@ -58,3 +65,27 @@ end
 
 % go!
 V = xplr.view(data,varargin{:});
+
+%---
+function xplor_window
+
+hf = fn_figure('[XPLOR]',[220 120]);
+delete(findall(hf,'parent',hf))
+
+uicontrol('unit','normalized','pos',[0 .6 1 .4], ...
+    'style','pushbutton','enable','inactive','fontsize',20, ...
+    'backgroundcolor',[.8 .8 1], ...
+    'string','XPLOR', ...
+    'callback','disp hello');
+uicontrol('unit','normalized','pos',[0 0 1 .6], ...
+    'style','edit','max',2,'fontsize',14, ...
+    'string','Drag & Drop variable here to XPLOR it', ...
+    'ForegroundColor',[1 1 1]*.5,'fontangle','italic', ...
+    'callback','disp hello');
+
+%     [~,~,~,dat] = flow;
+%     dat = permute(dat,[1 3 2]);
+%     s = size(dat);
+%     head = xplr.header({'x' 'px' s(1)},{'y' 'px' s(2)},{'axis' num2cell(['a':'y' 'A':'Y'])});
+%     data = xplr.xdata(dat,head,'Flow Data');
+
