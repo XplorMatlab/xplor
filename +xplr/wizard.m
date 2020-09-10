ecd422bc94381305274a692c4421623a9e8190d1
Thomas Deneux <thomas.deneux@cnrs.fr>
2020-09-10 15:54:01 +0200

    Create new folder +xplor for public API functions. Move demo functions there and create 'close_all_windows' function.

--- +xplr/wizard.m
+++ +xplr/wizard.m
@@ -2,6 +2,7 @@ function wizard(file)
 
 if nargin < 1
     file = brick.getfile('*.csv', 'Select data files to XPLOR');
+    if isequal(file, 0), return, end
 end
 data = io.read_table(file);
 xplor(data)
