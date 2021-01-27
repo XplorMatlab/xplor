dfb42a97f8d798066f1a2ae3c7426db3a2aed1ef
Thomas Deneux <thomas.deneux@cnrs.fr>
2021-01-27 18:52:27 +0100

    structedit: memorize previous returned value!

--- +brick/structedit.m
+++ +brick/structedit.m
@@ -1,12 +1,15 @@
-function s = structedit(s,varargin)
+function s_out = structedit(s,varargin)
 %STRUCTEDIT Edit a structure with a GUI brick.interface; this is a wrapper of brick.control
 %---
-% function s = structedit(s[,spec][,hp][,other control arguments...])
+% function s = structedit(s[,spec][,hp][,other control arguments...][,'memorize'])
 % function s = structedit(fieldname1,value1,fieldname2,value2,...)
 %---
-% allow user to interactively modify a structure, and returns the modified
+% Allow user to interactively modify a structure, and returns the modified
 % structure; returns an empty array if the figure is closed
-% this function is a wrapping of control
+% this function is a wrapping of control.
+% 'memorize' option will cause the result to be saved on the disk so that
+% if the function is called again with the same default value, this default
+% value will be replaced by the lastly saved value.
 % 
 % See all brick.control, brick.input, brick.reallydlg
 
@@ -28,17 +31,35 @@ if ischar(s)
     s = struct(structdef{:});
     varargin = {};
 end
+
+% replace default with previously memorized value?
+do_memorize = ~isempty(varargin) && strcmp(varargin{end}, 'memorize');
+if do_memorize
+    varargin(end) = []; 
+    mem_hash = ['structedit_' brick.hash(s(1))];
+    s_saved = brick.userconfig(mem_hash);
+    if ~isempty(s_saved)
+        s(1) = s_saved;
+    end
+end
 X = brick.control(s,varargin{:},'ok');
-addlistener(X,'OK',@(u,e)update());
-update()
-% wait that the figure will be destroyed
-s = [];
-waitfor(X.hp)
-drawnow % otherwise figure will stay on if heavy computation continue afterwards
+s_out = [];
 
 % nested function: update of s
 function update
-    s = X.s;
+    s_out = X.s;
+end
+
+addlistener(X,'OK',@(u,e)update());
+update()
+
+% wait that the figure will be destroyed
+waitfor(X.hp)
+drawnow % otherwise figure will stay on if heavy computation continue afterwards
+
+% memorize value?
+if do_memorize
+    brick.userconfig(mem_hash, s_out)    
 end
 
 end
